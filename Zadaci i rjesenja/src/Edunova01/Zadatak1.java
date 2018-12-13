cd171a2e44a61dfe20a0e558efff0b2022930fbb
Thinkpad <Thinkpad@Thinkpad-PC>
2018-12-13 14:15:32 +0100

    Zadaci i rjesenja

---
+++ Zadaci i rjesenja/src/Edunova01/Zadatak1.java
@@ -0,0 +1,11 @@
+package Edunova01;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello " + args[0]);
+
+	}
+
+}
---
+++ Zadaci i rjesenja/src/Edunova01/Zadatak2.java
@@ -0,0 +1,10 @@
+package Edunova01;
+
+public class Zadatak2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
